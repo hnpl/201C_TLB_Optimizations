881901e1623ae3f5996361f27033353744dddf5b
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-05-31 14:22:56 -0700

    miscs: Add index and address generators
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

---
+++ cache_model/pattern_generators/address_generator.py
@@ -0,0 +1,40 @@
+class AddressGenerator:
+    STATE_LOAD = 0
+    STATE_STORE = 1
+    def __init__(self, load_index_generator, store_index_generator, num_accesses, num_lanes, element_size_bytes = 8):
+        self.num_accesses = (num_accesses + num_lanes - 1) // num_lanes * num_lanes # round to the nearest integer this divisible by num_lanes
+        self.num_lanes = num_lanes
+        self.load_index_generator = load_index_generator(self.num_accesses)
+        self.store_index_generator = store_index_generator(self.num_accesses)
+        self.element_size_bytes = element_size_bytes
+        self.load_base = self.get_default_load_base()
+        self.store_base = self.get_default_store_base()
+        self.state = AddressGenerator.STATE_LOAD
+        self.lane_idx = 0
+    def get_default_load_base(self):
+        return 0
+    def get_default_store_base(self):
+        return 2**40 # 1 TiB apart seems to be sufficient
+    def _switch_state(self):
+        if self.state == AddressGenerator.STATE_LOAD:
+            self.state = AddressGenerator.STATE_STORE
+        elif self.state == AddressGenerator.STATE_STORE:
+            self.state = AddressGenerator.STATE_LOAD
+        else:
+            raise Error("Unknown state!")
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.lane_idx == self.num_lanes: # switch state
+            self.lane_idx = 0
+            self._switch_state()
+        vaddr = -1
+        if self.state == AddressGenerator.STATE_LOAD:
+            vaddr = self.load_base + next(self.load_index_generator) * self.element_size_bytes
+        elif self.state == AddressGenerator.STATE_STORE:
+            vaddr = self.store_base + next(self.store_index_generator) * self.element_size_bytes
+        else:
+            raise Exception("Unknown state!")
+        self.lane_idx += 1
+        return vaddr
+
---
+++ cache_model/pattern_generators/index_generator.py
@@ -0,0 +1,40 @@
+class IndexGenerator:
+    def __init__(self, num_accesses):
+        self.num_accesses = num_accesses
+        self.pattern = self.sequence_generator()
+    def sequence_generator(self):
+        raise NotImplementedError("All IndexGenerator objects must implement sequence_generator")
+    def __iter__(self):
+        return self
+    def __next__(self):
+        return next(self.pattern)
+
+class LinearGenerator(IndexGenerator):
+    def __init__(self, num_accesses):
+        super().__init__(num_accesses)
+    def sequence_generator(self):
+        for i in range(self.num_accesses):
+            yield i
+
+class StrideGenerator(IndexGenerator):
+    def __init__(self, num_accesses, stride, offset = 0):
+        super().__init__(num_accesses)
+        self.offset = offset
+        self.stride = stride
+    def sequence_generator(self):
+        for i in range(self.num_accesses):
+            yield self.offset + i * self.stride
+
+class MultiplicativeGenerator(IndexGenerator):
+    # multiplier should be a prime number such that multiplicative_order(Mod(multiplier, modulo)) == module - 1
+    # for page size of 4KiB and element size of 8 bytes, multiplier should be bigger than 512 so that (almost) all adjacent indices will touch different pages
+    def __init__(self, num_accesses, multiplier = 523, modulo = 10**7 + 79):
+        super().__init__(num_accesses)
+        self.multiplier = multiplier
+        self.modulo = modulo
+    def sequence_generator(self):
+        w = 1
+        for i in range(self.num_accesses):
+            w = (w * self.multiplier) % self.modulo
+            yield w
+
