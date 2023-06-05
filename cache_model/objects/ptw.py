35598f5476f1c05d71c969421cdc5eab9b984721
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-06-05 00:14:33 -0700

    miscs: Add PooledPTWs3
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- cache_model/objects/ptw.py
+++ cache_model/objects/ptw.py
@@ -1,7 +1,7 @@
 from math import log2
 
 from .device import Device
-from .tlb_cache_entry import TLBCacheEntry
+from .data_cache import DataCache
 
 def page_size_bytes_to_num_page_table_levels(page_size_bytes):
     num_offset_bits = round(log2(page_size_bytes))
@@ -159,7 +159,6 @@ class PooledPTWs1(PooledPTWs):
 class PooledPTWs2(PooledPTWs):
     def __init__(self, name, page_table_size = 2**12):
         super().__init__(name, page_table_size)
-        self.count = 0
     def add_path_to_graph(sub_vpns, vpn_graph_root):
         w = vpn_graph_root
         for sub_vpn in sub_vpns:
@@ -176,13 +175,72 @@ class PooledPTWs2(PooledPTWs):
         while to_be_visited:
             node = to_be_visited.pop(0)
             for sub_vpn, child_node in node.items():
-                self.count += 1
                 self.access_memory(sub_vpn)
                 to_be_visited.append(child_node)
         self.requests = []
 
 class PooledPTWs3(PooledPTWs):
-    def __init__(self, name, page_table_size = 2**12):
+    def __init__(self, name, memory_backend, page_table_size = 2**12):
         super().__init__(name, page_table_size)
+        # Since we are simulating going to actual addresses in memory with a cache system, we have to provide somewhat accurate addresses
+        # Assume that each page table entry is of size 8 bytes, then the locations of the page table entry of a vaddr are at,
+        #   Level 1 page table entry: satp + vpn0 * 8
+        #   Level 2 page table entry: satp + (2**9) * 8 + cat(vpn0, vpn1) * 8
+        #   Level 3 page table entry: satp + (2**9 + 2**18) * 8 + cat(vpn0, vpn1, vpn2) * 8
+        #   Level 4 page table entry: satp + (2**9 + 2**18 + 2**27)*8 + cat(vpn0, vpn1, vpn2, vpn3) * 8
+        # Let satp = 1 << 32
+        # Then the max page table entry address is [(1 << 32) + (2**9 + 2**18 + 2**27)*8 + ((1<<36) - 1)*8], which is presentable using 40 bits
+        self.satp = 1 << 32
+        # Add a small cache caching Level 1 page table entry
+        self.l1_pte_cache = DataCache(name = "l1_pte_cache", num_entries = 32, associativity = 8, block_size_bytes = 64)
+        self.l1_pte_cache.connect_lower_level_device(memory_backend)
+        memory_backend.connect_higher_level_device(self.l1_pte_cache)
+        self.add_child_device(self.l1_pte_cache)
+        # Add a small cache caching Level 2 page table entry
+        self.l2_pte_cache = DataCache(name = "l2_pte_cache", num_entries = 32, associativity = 8, block_size_bytes = 64)
+        self.l2_pte_cache.connect_lower_level_device(memory_backend)
+        memory_backend.connect_higher_level_device(self.l2_pte_cache)
+        self.add_child_device(self.l2_pte_cache)
+        # Add a small cache caching Level 3 page table entry, this causes a lot of misses
+        #self.l3_pte_cache = DataCache(name = "l3_pte_cache", num_entries = 32, associativity = 8, block_size_bytes = 64)
+        #self.l3_pte_cache.connect_lower_level_device(memory_backend)
+        #memory_backend.connect_higher_level_device(self.l3_pte_cache)
+        #self.add_child_device(self.l3_pte_cache)
+    def add_path_to_graph(sub_vpns, vpn_graph_root):
+        w = vpn_graph_root
+        concat_vpn = 0
+        for level, sub_vpn in enumerate(sub_vpns):
+            # different from PooledPTWs3, here we concat the vpn
+            # e.g. (vpn0, vpn1, vpn2, vpn3) = (1, 32, 85, 92)
+            # then, concat_vpn0 = 1
+            #       concat_vpn1 = (1 << 9) | 32
+            #       concat_vpn2 = (concat_vpn1 << 9) | 85
+            #       concat_vpn3 = (concat_vpn2 << 9) | 92
+            concat_vpn = (concat_vpn << 9) | (sub_vpn)
+            if not (level, concat_vpn) in w:
+                w[(level, concat_vpn)] = {}
+            w = w[(level, concat_vpn)]
     def make_progress(self):
+        vpn_graph_root = {}
+        for vaddr in self.requests:
+            sub_vpns = parse_vaddr(vaddr, self.page_size_bytes)
+            PooledPTWs3.add_path_to_graph(sub_vpns, vpn_graph_root)
+        # using BFS to traverse the vpn graph and making one memory request per node
+        to_be_visited = [vpn_graph_root]
+        while to_be_visited:
+            node = to_be_visited.pop(0)
+            for level_concat_vpn, child_node in node.items():
+                level, concat_vpn = level_concat_vpn
+                if level == 1:
+                    pte_address = self.satp + concat_vpn * 8
+                    self.l1_pte_cache.receive_request_and_send_response(pte_address)
+                elif level == 2:
+                    pte_address = self.satp + (2**9) * 8 + concat_vpn * 8
+                    self.l2_pte_cache.receive_request_and_send_response(pte_address)
+                #elif level == 3:
+                #    pte_address = self.satp + (2**9 + 2**18) * 8 + concat_vpn * 8
+                #    self.l3_pte_cache.receive_request_and_send_response(pte_address)
+                else:
+                    self.access_memory(concat_vpn)
+                to_be_visited.append(child_node)
         self.requests = []
