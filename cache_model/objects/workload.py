4ee69eb04b91713ed92698ab19deb63b5a1fd53d
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-06-06 14:49:15 -0700

    miscs: Add STREAM, GUPS, and PermutatingGather workloads
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

---
+++ cache_model/objects/workload.py
@@ -0,0 +1,34 @@
+class Workload:
+    def __init__(self, num_lanes, array_size):
+        self.num_lanes = num_lanes
+        self.array_size = (array_size + num_lanes - 1) // num_lanes * num_lanes
+    def __iter__(self):
+        return self
+    def __next__(self):
+        raise NotImplementedError("__next__() must be implemented for all Workload objects")
+
+# A Workload that can change its state.
+# Each address generator generates a stream of indices for an array, and a state is associated with an array.
+# For each state, the workload will trigger a different indices generator according to the states_generators_map.
+# The array_bases map contains the base address of each array (or state).
+class PhasedWorkload(Workload):
+    def __init__(self, num_lanes, array_size, states_index_generators_map, array_bases, element_size_bytes, starting_state):
+        super().__init__(num_lanes, array_size)
+        self.states_index_generators_map = states_index_generators_map
+        self.array_bases = array_bases
+        self.element_size_bytes = element_size_bytes
+        self.lane_idx = 0
+        self.state = starting_state
+    def next_state(self, current_state):
+        raise NotImplementedError("next_state() must be implemented for all PhasedWorkload objects")
+    def __next__(self):
+        if self.lane_idx == self.num_lanes:
+            self.lane_idx = 0
+            self.state = self.next_state(self.state)
+        vaddr = -1
+        array_offset = next(self.states_index_generators_map[self.state])
+        array_base = self.array_bases[self.state]
+        vaddr = array_base + array_offset * self.element_size_bytes
+        self.lane_idx += 1
+        assert((vaddr < (1 << 48)) and "We are assuming SV48, the vaddr should be smaller than 2**48")
+        return vaddr
---
+++ cache_model/workloads/stream.py
@@ -0,0 +1,58 @@
+import sys
+sys.path.append("../")
+
+from objects.workload import PhasedWorkload
+from objects.pattern_generators.index_generator import LinearIndexGenerator
+
+class StreamCopyWorkload(PhasedWorkload):
+    def __init__(self, num_lanes, array_size, element_size_bytes):
+        self.state_load = 0
+        self.state_store = 1
+        states_index_generators_map = {} # we will add the generators later after we call the parent constructors,
+                                         # which will fix the array_size so that array_size is a multiple of num_lanes
+        array_bases = { self.state_load: 0,
+                        self.state_store: (array_size + 2**12) * element_size_bytes }
+        starting_state = self.state_load
+        super().__init__(num_lanes = num_lanes,
+                         array_size = array_size,
+                         states_index_generators_map = states_index_generators_map,
+                         array_bases = array_bases,
+                         element_size_bytes = element_size_bytes,
+                         starting_state = starting_state)
+        load_index_generator = LinearIndexGenerator(self.array_size)
+        store_index_generator = LinearIndexGenerator(self.array_size)
+        states_index_generators_map[self.state_load] = load_index_generator
+        states_index_generators_map[self.state_store] = store_index_generator
+    def next_state(self, current_state):
+        next_state_map = { self.state_load: self.state_store,
+                           self.state_store: self.state_load }
+        return next_state_map[current_state]
+
+class StreamAddWorkload(PhasedWorkload):
+    def __init__(self, num_lanes, array_size, element_size_bytes):
+        self.state_load_1 = 0
+        self.state_load_2 = 1
+        self.state_store = 2
+        array_bases = { self.state_load_1: 0,
+                        self.state_load_2: (array_size + 2**12) * element_size_bytes,
+                        self.state_store:  2 * (array_size + 2**12) * element_size_bytes }
+        states_index_generators_map = {}
+        starting_state = self.state_load_1
+        super().__init__(num_lanes = num_lanes,
+                         array_size = array_size,
+                         states_index_generators_map = states_index_generators_map,
+                         array_bases = array_bases,
+                         element_size_bytes = element_size_bytes,
+                         starting_state = starting_state)
+        load_1_index_generator = LinearIndexGenerator(self.array_size)
+        load_2_index_generator = LinearIndexGenerator(self.array_size)
+        store_index_generator = LinearIndexGenerator(self.array_size)
+        states_index_generators_map[self.state_load_1] = load_1_index_generator
+        states_index_generators_map[self.state_load_2] = load_2_index_generator
+        states_index_generators_map[self.state_store] = store_index_generator
+    def next_state(self, current_state):
+        next_state_map = { self.state_load_1: self.state_load_2,
+                           self.state_load_2: self.state_store,
+                           self.state_store: self.state_load_1 }
+        return next_state_map[current_state]
+
