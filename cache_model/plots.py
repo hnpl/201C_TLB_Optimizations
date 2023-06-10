0c06f1c5cd4c5e0d73d510b07901176d12e92c2f
ShaneXiangH <823473811@qq.com>
2023-06-09 21:24:10 -0400

    Add by workload, by lanes, by page size ploting

--- cache_model/plots.py
+++ cache_model/plots.py
@@ -8,20 +8,23 @@ import numpy as np
 
 eps = 0.000000001
 
-def extract_variables(filename):
+def extract_variables(filename: str) -> [int, int, str, int, str]:
 
     print('extracting file: {}'.format(filename))
+    stream_class = None
     info = filename.split('_')
     design_class, page_size_bytes, workload_class = info[1], info[2], info[3]
     if (workload_class == 'gemm') or (workload_class == 'gups'):
         num_lanes = info[4]
     elif (workload_class == 'npb') or (workload_class == 'permutating') or (workload_class == 'stream'):
         num_lanes = info[5]
+        if (workload_class == 'stream'):
+            stream_class = info[4]
     else:
         raise NotImplementedError
     design_class, page_size_bytes, num_lanes = int(design_class), int(page_size_bytes), int(num_lanes)
 
-    return design_class, page_size_bytes, workload_class, num_lanes
+    return design_class, page_size_bytes, workload_class, num_lanes, stream_class
 
 
 def extract_design_1(lines, num_lanes):
@@ -92,18 +95,65 @@ def extract_design_4(lines, num_lanes):
 
     return l1_pte_cache_missrate, l2_pte_cache_missrate, l3_pte_cache_missrate, ptw_pool_missrate, l2_tlb_missrate, address_coalescer_missrate, l1_tlb_missrate
 
+workload_matcher = {'gemm': 0, 'gups': 1, 'npb': 2, 'permutating': 3, 'stream': 4}
+stream_matcher = {'add': 4, 'copy': 5}
+lane_matcher = {'8': 0, '32': 1, '128': 2}
+page_size_matcher = {'4096': 0, '2097152': 1}
 
-def get_tlb_missrates():
+def get_workload_index(workload_class, stream_class):
+
+    index = workload_matcher[workload_class]
+    assert index in [0, 1, 2, 3, 4]
+    if index == 4:
+        index = stream_matcher[stream_class]
+    assert index in [0, 1, 2, 3, 4, 5]
+    
+    return index
+
+def get_lane_index(num_lanes):
+    return lane_matcher[num_lanes]
+
+def get_page_size_index(page_size_bytes):
+    return page_size_matcher[page_size_bytes]
+
+def get_avg(a: list) -> float: 
+    return sum(a) / len(a)
+
+def data_post_processing():
 
     print('***Started Data Loading***')
     nums_foreach_design = [0, 0, 0, 0]
-    l1_tlb_missrate_global = [[], [], [], []]  # for each design respectively
+    l1_tlb_missrate_global = [[], [], [], []]
     l2_tlb_missrate_global = [[], [], [], []]  # for each design respectively
+    l1_tlb_missrate_by_workload = [[[], [], [], [], [], []],
+                                    [[], [], [], [], [], []],
+                                    [[], [], [], [], [], []],
+                                    [[], [], [], [], [], []]]
+    l2_tlb_missrate_by_workload = [[[], [], [], [], [], []],
+                                    [[], [], [], [], [], []],
+                                    [[], [], [], [], [], []],
+                                    [[], [], [], [], [], []]]  # [design_class, workload_class]
+    l1_tlb_missrate_by_num_lanes = [[[], [], []],
+                                    [[], [], []], 
+                                    [[], [], []], 
+                                    [[], [], []]]
+    l2_tlb_missrate_by_num_lanes = [[[], [], []],
+                                    [[], [], []], 
+                                    [[], [], []], 
+                                    [[], [], []]] # [design_class, num_lanes_class]
+    l1_tlb_missrate_by_page_size = [[[], []],
+                                    [[], []], 
+                                    [[], []], 
+                                    [[], []]] 
+    l2_tlb_missrate_by_page_size = [[[], []],
+                                    [[], []], 
+                                    [[], []], 
+                                    [[], []]] # [design_class, page_size_class]
     data_folder = os.path.join(os.getcwd(), 'cache_model/results')
     for filename in os.listdir(data_folder):
         if filename.split('_')[0] != 'design':
             continue
-        design_class, page_size_bytes, workload_class, num_lanes = extract_variables(filename)
+        design_class, page_size_bytes, workload_class, num_lanes, stream_class = extract_variables(filename)
         nums_foreach_design[design_class-1] = nums_foreach_design[design_class-1] + 1
         with open(os.path.join(data_folder, filename), 'r') as f:
             lines = f.readlines()
@@ -117,12 +167,37 @@ def get_tlb_missrates():
                 raise NotImplementedError
             l1_tlb_missrate_global[design_class-1].append(l1_tlb_missrate)
             l2_tlb_missrate_global[design_class-1].append(l2_tlb_missrate)
+            index = get_workload_index(workload_class, stream_class)
+            l1_tlb_missrate_by_workload[design_class-1][index].append(l1_tlb_missrate)
+            l2_tlb_missrate_by_workload[design_class-1][index].append(l2_tlb_missrate)
+            lane_index = get_lane_index(str(num_lanes))
+            l1_tlb_missrate_by_num_lanes[design_class-1][lane_index].append(l1_tlb_missrate)
+            l2_tlb_missrate_by_num_lanes[design_class-1][lane_index].append(l2_tlb_missrate)
+            page_size_index = get_page_size_index(str(page_size_bytes))
+            l1_tlb_missrate_by_page_size[design_class-1][page_size_index].append(l1_tlb_missrate)
+            l2_tlb_missrate_by_page_size[design_class-1][page_size_index].append(l2_tlb_missrate)
         
     for i in range(4):
-        l1_tlb_missrate_global[i] = sum(l1_tlb_missrate_global[i]) / len(l1_tlb_missrate_global[i])
-        l2_tlb_missrate_global[i] = sum(l2_tlb_missrate_global[i]) / len(l2_tlb_missrate_global[i])
+        l1_tlb_missrate_global[i] = get_avg(l1_tlb_missrate_global[i])
+        l2_tlb_missrate_global[i] = get_avg(l2_tlb_missrate_global[i])
+        for j in range(6):
+            l1_tlb_missrate_by_workload[i][j] = get_avg(l1_tlb_missrate_by_workload[i][j])
+            l2_tlb_missrate_by_workload[i][j] = get_avg(l2_tlb_missrate_by_workload[i][j])
+        for j in range(3):
+            l1_tlb_missrate_by_num_lanes[i][j] = get_avg(l1_tlb_missrate_by_num_lanes[i][j])
+            l2_tlb_missrate_by_num_lanes[i][j] = get_avg(l2_tlb_missrate_by_num_lanes[i][j])
+        for j in range(2):
+            l1_tlb_missrate_by_page_size[i][j] = get_avg(l1_tlb_missrate_by_page_size[i][j])
+            l2_tlb_missrate_by_page_size[i][j] = get_avg(l2_tlb_missrate_by_page_size[i][j])
+    
     print('l1_tlb_missrate_global: {}'.format(l1_tlb_missrate_global))
     print('l2_tlb_missrate_global: {}'.format(l2_tlb_missrate_global))
+    print('l1_tlb_missrate_by_workload: {}'.format(l1_tlb_missrate_by_workload))
+    print('l2_tlb_missrate_by_workload: {}'.format(l2_tlb_missrate_by_workload))
+    print('l1_tlb_missrate_by_num_lanes: {}'.format(l1_tlb_missrate_by_num_lanes))
+    print('l2_tlb_missrate_by_num_lanes: {}'.format(l2_tlb_missrate_by_num_lanes))
+    print('l1_tlb_missrate_by_page_size: {}'.format(l1_tlb_missrate_by_page_size))
+    print('l2_tlb_missrate_by_page_size: {}'.format(l2_tlb_missrate_by_page_size))
     print('***Finished Data Loading***')
 
-    return l1_tlb_missrate_global, l2_tlb_missrate_global
+    return l1_tlb_missrate_global, l2_tlb_missrate_global, l1_tlb_missrate_by_workload, l2_tlb_missrate_by_workload, l1_tlb_missrate_by_num_lanes, l2_tlb_missrate_by_num_lanes, l1_tlb_missrate_by_page_size, l2_tlb_missrate_by_page_size
